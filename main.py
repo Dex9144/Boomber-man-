02c13049d00d81bf2391ec5768ff671648d131a6
Dexit9144 <daniel0453@icloud.com>
2024-02-22 19:55:57 +0100

    v3

--- main.py
+++ main.py
@@ -5,17 +5,29 @@ SCREEN_HEIGHT = 660
 SCREEN_TITLE = "Bomberman"
 
 
+"""Bg settings"""
+CELL_WIDTH = 60
+CELL_HEIGHT = 60
+
+ROW_COUNT = 11
+COL_COUNT = 11
+
+
 class Game(arcade.Window):
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.setup()
 
+        self.bg = arcade.load_texture("Blocks/BackgroundTile.png")
+
     def setup(self):
         pass
 
     def on_draw(self):
         self.clear()
+        self.draw_background()
+
 
     def update(self, delta_time):
         pass
@@ -26,6 +38,10 @@ class Game(arcade.Window):
     def on_key_release(self, key, modifiers):
         pass
 
+    def draw_background(self):
+        for y in range(ROW_COUNT):
+            for x in range(COL_COUNT):
+                arcade.draw_texture_rectangle()
 
 window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
